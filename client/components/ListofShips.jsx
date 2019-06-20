b7beaec204b98161a55ee10582f0a78fb8286303
robjwilkinson@hotmail.com <robert@Kaias-iPod-2.localdomain>
2019-06-20 16:41:39 +1200

    Start of Heading onClick to GitHub - 20th june

--- client/components/ListofShips.jsx
+++ client/components/ListofShips.jsx
@@ -75,12 +75,21 @@ class ListofShips extends React.Component {
     
       <React.Fragment>
 
-        {/* <h3>Testing Cruise Line - (Princess)</h3> */}
+        {/* There will be Headings for all the Cruise Lines.
+        When a Cruise Line Heading is clicked, the Ships List opens up for that Heading.
+        When user clicks on a Cruise Line Heading, when a Ships List is open, the Ships List Collapses.  
+           */}
+        <h3 onClick = "this.handleClick">{cruise_line}</h3>
 
         {this.state.shipList.map (ship => {  
             
-          return  <h3 onClick = "this.handleClick">{cruise_line}</h3> 
-                  <li className = "shipList" key = {ship.cruise_line}><Link to = {`/cruiselines/${cruise_line#ship_name}`}>{this.findShipName(cruise_line)}</Link></li>
+          return   <li className = "shipList" key = {ship.cruise_line}><Link to = {`/cruiselines/${cruise_line#ship_name}`}>{this.findShipName(cruise_line)}</Link></li>
+          
+          // I have to find a key that can display each ship name for each Headings Ships List.
+          // The Link will be an Anchor Link to that specific ship, on it's Cruise Line Page.
+          // Note: The Cruise Line Page, can have a Review for more than one ship and will be a Review Component.   
+
+
         })
         }
 
