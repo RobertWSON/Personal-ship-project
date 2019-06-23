5e9f3ebe41376929154f8e98946f4afc2439399c
robjwilkinson@hotmail.com <robert@Roberts-Air.lan>
2019-06-23 22:11:49 +1200

    Trying to require Cruises js Routes into Server js, but it does not allow it. So no changes to File

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+//const ships = require('../routes/cruises')
 const ships = require('./routes/ships')
 
 const server = express()
@@ -8,6 +9,7 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
 server.use('/v1/review', ships)
+//server.use('v1/review', cruiseline)
 server.use('/v1/*', (req, res) => res.sendStatus(404))
 
 //server.use('api/ships', require ('./routes/review'))
