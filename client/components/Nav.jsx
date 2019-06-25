141133f8172bf05759b8718789657fb23b3c6e10
robjwilkinson@hotmail.com <robert@Kaias-iPod-2.localdomain>
2019-06-26 11:19:47 +1200

    Trying to adjust images sizes on evolution pages - not yet even in height

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,10 +3,11 @@ import {Link} from 'react-router-dom'
 
 
 const Nav = () => {
-
+     
     return (
     // <div className = 'title'>
     //     <h2>Cruise Shipin</h2>
+
         <div className = 'navmenu'>    
             {/* <h3><Link to="/home" className={'navoptions'}>Home</Link></h3>
             <h3><Link to="/evolution-part1" className={'navoptions'}>Evolution (Part 1)</Link></h3>
@@ -14,7 +15,9 @@ const Nav = () => {
             <h3><Link to="/cruise-lines" className={'navoptions'}>Cruise Lines</Link></h3>
             <h3><Link to="Reviews" className={'navoptions'}>Reviews</Link></h3> */}
 
-            <h2 className ="top-links"><Link to={"/"}>Home</Link>
+            {/* <h2 className ="top-links"> */}
+            <h2>
+                <Link to={"/"}>Home</Link>
                 <Link to={"/evolutionpart1"}>Evolution (Part 1)</Link>
                 <Link to={"/evolutionpart2"}>Evolution (Part2)</Link>
                 <Link to={"/cruiselines"}>Cruise Lines</Link>
