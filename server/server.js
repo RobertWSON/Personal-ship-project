704a55c644652c7d9d0a36e919185da164576ddf
robjwilkinson@hotmail.com <robert@Kaias-iPod-2.localdomain>
2019-06-26 13:34:42 +1200

    Routes & Server js constants

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-//const cruiselines = require('../routes/cruises')
+const cruiseline = require('./routes/cruises')
 const ships = require('./routes/ships')
 
 const server = express()
@@ -9,7 +9,7 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
 server.use('/v1/review', ships)
-//server.use('v1/review', cruiseline)
+server.use('v1/review', cruiseline)
 server.use('/v1/*', (req, res) => res.sendStatus(404))
 
 //server.use('api/ships', require ('./routes/review'))
