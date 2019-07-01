f6212ab98d2bc21f58c323205e0247b6222a6c52
Rich Churcher <rich.churcher@gmail.com>
2019-07-02 07:30:44 +1200

    Update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: './client/index.js',
+  entry: "./client/index.js",
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
-  devtool: 'source-map'
-}
+  devtool: "source-map"
+};
