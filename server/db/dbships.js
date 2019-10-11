1056a0009a03ee05076e05e05c44b11f19e7f9b7
robjwilkinson@hotmail.com <robert@Roberts-MacBook-Air.local>
2019-10-11 23:16:15 +1300

    server/db/dbships.js - getShipsList function now has .where and cruiseline

--- server/db/dbships.js
+++ server/db/dbships.js
@@ -3,16 +3,16 @@ const config = require('../../knexfile')[environment]
 const db = require('knex')(config)
 
 
-function getShipsList(testConn)   {
+function getShipsList(cruise_line, testConn)   {
     const conn = testConn || db
     console.log('server/db/dbships.js getShipsList called!')
 
     //Line below connects to Ships Table
     return conn('Ships')
-    //Line below picks out each Cruise Line to get the Ships from  
-    .distinct('cruise_line')
     //Line below picks out the Ships to display for each Cruise Line
     .select('id','cruise_line','ship_name')
+    //Line below picks up the Cruise line from where the Ships names have come from
+    .where('cruise_line', cruise_line)
     //Line below puts Ships in Alphabetical Order.
     .orderBy("ship_name")    
     .then(dbResult  =>  {
