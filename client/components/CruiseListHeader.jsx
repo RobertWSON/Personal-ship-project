d06b49f7d8e67e241df93696c1cd09baf2cce22e
robjwilkinson@hotmail.com <robert@Roberts-Air.lan>
2019-10-14 00:35:00 +1300

    CruiseListHeader & ListofShips Components - Changed so that state for opening and closing each Cruise Line Heading is controlled in ListofShips Component

--- client/components/CruiseListHeader.jsx
+++ client/components/CruiseListHeader.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import {getCruiseLines} from '../api/api'
-//import {getShipsList} from '../api/api'
 import ListofShips from './ListofShips'
-//import {CruiseListHeader} from './CruiseListHeader'
 
 
 class CruiseListHeader extends React.Component {
     constructor(props)  {
         super(props)
 
-        //setting intial state for Cruise Heading and shipsList and initialize cruiseHeaders as an empty array
+        //setting intial state for Cruise Headings and initialize cruiseHeaders as an empty array
         this.state = {
-            cruiseHeaders: [],
-            shipsList: {isOpen: false}
+            cruiseHeaders: []
         } 
         
-        //binding methods for Cruise Line Headers and Handle Click Function
-        this.setUpCruiseLines = this.setUpCruiseLines.bind(this),
-        this.handleClick = this.handleClick.bind(this)
+        //binding methods for setting up Cruise Line Headings
+        this.setUpCruiseLines = this.setUpCruiseLines.bind(this)
     }  
 
     componentDidMount() {
@@ -35,53 +31,33 @@ class CruiseListHeader extends React.Component {
                 })
             })
     }
-    
-    /* There will be Headings for all the Cruise Lines.
-        When a Cruise Line Heading is clicked, it goes to ListofShips Component and the Ships List opens up for that Heading.
-        When a Ships List is open and user clicks on a Cruise Line Heading, the Ships List Collapses
-        and just a Cruise Line Heading is shown.*/
-      
-    handleClick(event)   {
-        //This handleClick sorts out the changing of state between Shipslist being open or not, when a Cruise Line Heading is clicked.
-        //HandleClick function should only handle 'isOpen' value in the state
-        //and any renders are to be made on the 'render' method instead.
-
-        this.setState(prevState =>  ({
-            shipsList:  {
-                //will reverse prevState of isOpen.
-                isOpen: !prevState.shipsList.isOpen,
-            }
-        }))
-    }
-
      
     render()    {
-        //This renders at the start when page loads and also when you close a Ships List
-        //We can handle the way you render the component that depends on the this.state.shipsList.isOpen in the code below. 
-        //This is a destructive declaration for isOpen from inside shipsList in the state that does this.
-        const {shipsList: {isOpen}} = this.state
 
         return  (
 
             <React.Fragment>
 
-                {/* Map the Cruiseline Headings for each Ship to display them on the page
-                I want to map ship because I need each ship displayed in a List for a Cruise Line when isOpen is True.
-                Each different Cruise line is showing, so that is good, but not opening Ships List.*/}
-                {this.state.cruiseHeaders.map (ship =>  (
+                {/* There will be Headings for all the Cruise Lines. */}
+                {/* Map the Cruiseline Headings for each Ship to display them on the page.
+                I want to map ship, because I need each ship displayed in a List, when Cruise Line Heading is clicked.
+                Each different Cruise line is showing, so that is excellent. 
+                I just have to get it displaying in rows across the page now.*/}
+                {this.state.cruiseHeaders.map (ship =>  {
                 
-                    <div key={ship.cruise_line}>
-                        
-                            {/* Line below is a Conditonal Operator that displays ShipsList when isOpen is True */}
-                            {isOpen == true && <ListofShips/>}
-                        
-                        <h3>
-                            <button onClick = {this.handleClick}>{ship.cruise_line}</button>
-                        </h3>
-                    </div>
+                    return (
 
-                ))
-                }  
+                    <div key={ship.cruise_line}>
+
+                        {/* ListofShips component needs cruise_line, because when user clicks on Cruise Line Heading button,
+                            we need to fetch ships that belongs to that particular cruiseline. */}
+                        {/* We need to render multiple ListofShips components, with one for each cruiseline */}
+                        <ListofShips cruise_line={ship.cruise_line}></ListofShips>
+
+                    </div>
+                    )
+
+                })}  
 
             </React.Fragment> 
         )
--- client/components/ListofShips.jsx
+++ client/components/ListofShips.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {getShipsList} from '../api/api'
 import {HashLink as Link} from 'react-router-hash-link'
-//import {ListofShips} from './ListofShips'
 
 
 class ListofShips extends React.Component {
@@ -10,11 +9,16 @@ class ListofShips extends React.Component {
   
       this.state = {
         //initialize shipsList as an empty array
-        shipsList: []
-         
+        shipsList: [],
+        //This is false because the shipList is not initially open.
+        isOpen: false,
+        //This is initially false because we have not visited shipsList yet. 
+        hasfetched: false 
       }
-      //binding method for Ships in their List for their Cruise Line
-      this.setUpShips = this.setUpShips.bind(this)
+      //binding method for setting up Ships in their List for their Cruise Line
+      this.setUpShips = this.setUpShips.bind(this),
+      //binding method for HandClick function, which controls clicking on a Cruise Line Heading
+      this.handleClick = this.handleClick.bind(this)
     }
    
     componentDidMount(){
@@ -24,14 +28,42 @@ class ListofShips extends React.Component {
 
     setUpShips() {
       console.log('getShipsList')
-      getShipsList()
+      //Line below gets the Ships List function and CruiseLine parameter passed as a property from the api. 
+      getShipsList(this.props.cruise_line)
       
       .then(res =>  {
         
         this.setState({
-          shipsList: res  
+          shipsList: res,
+          //This saves us from making an api call , when the CruiseLine heading has already been visited. 
+          hasFetched: true  
         })
       })
+    }
+
+    /* When a Ships List is open and user clicks on a Cruise Line Heading, the Ships List Collapses
+       and just a Cruise Line Heading is shown.*/
+
+    handleClick() {
+   
+    //This handleClick sorts out the changing of state between Shipslist being open or not, when a Cruise Line Heading is clicked.
+    
+    //HandleClick function should only handle 'isOpen' value in the state
+
+      //This if statement finds out if the shipsList has been visited and if it has, it uses hasFetched to find Ships for List.
+      if(!this.state.hasFetched)  {
+        this.setUpShips()
+      }
+
+      //This block of code below sorts out the toggling of the State for the Cruise Line Headings. 
+      this.setState(prevState =>  ({
+        //Code below will find previous state of Ships List when it is either open or closed.
+        shipsList: prevState.shipsList,
+        //Code below will reverse the Previous State of isOpen for Ships List when it is open or closed.
+        isOpen: !prevState.isOpen,
+        //Code below will find Previous State of hasFetched for Ships List when it is closed.
+        hasFetched: prevState.hasFetched
+      }))
     }
 
 
@@ -39,8 +71,8 @@ class ListofShips extends React.Component {
 
       return ship.ship_name
 
-      //This function helps display relevant Cruise Ships when user clicks on their Cruise Line
-      //In my backend I have let the database find the relevant Cruise Ships from their Cruise Line, so we just return it here
+      //This function helps display relevant Cruise Ships Names when user clicks on a Cruise Line Heading.
+      //In my backend I have let the database find the relevant Cruise Ships from their Cruise Line, so we just return it here.
     }
     
   render()  {
@@ -49,18 +81,27 @@ class ListofShips extends React.Component {
     
       <React.Fragment>
 
-      <ol>
-        {this.state.shipsList.map (ship => {  
-          return   (
-              <li className = "shipsList" key = {ship.ship_name}><Link smooth to = {`/cruiselines/${ship.cruise_line}#${ship.ship_name}`}>{this.findShipNames(ship)}</Link></li>
-          )  
+        {/*This is the button for each Cruise line Heading and the state of it will be controlled by the handleClick function.*/}
+        {/*What is displayed is the result of the CruiseLine parameter passed as a property from the api*/}
+        <button onClick={this.handleClick}>{this.props.cruise_line}</button>
+        <ol>
+    
+          {/* When Ships List is about to be opened, we will use map to find the correct Ships for the List.*/}
+          {this.state.isOpen == true && 
+            this.state.shipsList.map (ship => {  
+              return   (
+                //We only need to find the id for each ship, because the api is doing the rest of the work for us by finding the 
+                //correct Cruise line to match the id. 
+                <li className = "shipsList" key = {ship.id}>
+                  <Link smooth to = {`/cruiselines/${ship.cruise_line}#${ship.ship_name}`}>{this.findShipNames(ship)}</Link>
+                </li>
+              )  
 
-          //This List has a className, because I want to style fonts using CSS for CruiseShip names 
+          //This List has a className, because I want to style fonts using CSS for Cruise Ship Names. 
           //The key is used to get correct ship names for the Ships List, when a Cruise Line Heading is clicked.
           //The Link will be an Anchor Link to a specific ship, on it's Cruise Line Page. 
           //The Link will have a smooth feature, so it scrolls to the correct ship on it's Cruise Line Page. 
           // Note: The Cruise Line Page, can have a Review for more than one ship and will be a Review Component.   
-        
         
         })
         }
