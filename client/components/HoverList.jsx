004cf9b150675a74698ad69d61a0669ae19fd1b7
robjwilkinson@hotmail.com <robert@Roberts-Air.lan>
2020-03-06 12:58:10 +1300

    ListofShips & HoverList - passing props not yet working

--- client/components/HoverList.jsx
+++ client/components/HoverList.jsx
@@ -7,12 +7,8 @@ class HoverList extends React.Component {
         super(props)
 
         this.state = {
-            //initialize shipsList as an empty array
-            shipsList: [],
             //This is false because the user has not initially Hovered over Link.
-            isHover: false,
-            //This is initially false because we have not visited shipsList yet. 
-            hasfetched: false 
+            isHover: false
         }
         this.onMouseOut = this.onMouseOut.bind(this)
         this.onMouseOver = this.onMouseOver.bind(this)
@@ -20,20 +16,21 @@ class HoverList extends React.Component {
 
 
     onMouseOut()  {
-
+        this.setState({isHover: false})
     }
 
     onMouseOver()   {
 
-        this.setState = ({
-            isHover:true
+        this.setState ({
+            isHover: true 
+            && <img src = {this.props.shipImage}/>
         })
 
     }
 
-    findShipNames(ship) {
+    // findShipNames(ship) {
 
-        return ship.ship_name
+    //     return ship.ship_name
   
         //This function helps display relevant Cruise Ships Names when user clicks on a Cruise Line Heading.
         //In my backend I have let the database find the relevant Cruise Ships from their Cruise Line, so we just return it here.
@@ -45,50 +42,29 @@ class HoverList extends React.Component {
 
             <React.Fragment>
 
-                <ol>
-                    
-                    {/* When Ships List is about to be opened, we will use map to find the correct Ships for the List.*/}
-                    {this.state.isOpen == true && 
-                    this.state.shipsList.map (ship => { 
-                        
-                        // Below we are creating constants that will be used in the Link below.
+                {/* Link in Cruise Lines Page to Review Page now works, because we have used encoding referenced from constants above. 
+                So we now have a HashLink with a hash and underscore in it.   */}
 
-                        // The CruiseLine and ShipName constants are used to replace all spaces in the url with an underscore "_".
-                        const encodedCruiseline = ship.cruise_line.replace(/\s/g, '_')
-                        const encodedShipName = ship.ship_name.replace(/\s/g, '_')
+                {/* Comment below as it has to be changed */}
+                {/* <Link className = "shipsList" smooth to = {`/cruiselines/${encodedCruiseline}#${encodedShipName}`}
+                    onMouseOut = {this.onMouseOut} onMouseOver = {this.onMouseOver}>
 
-                        // replace function takes two arguments, first argument for matching pattern (what to replace) and 
-                        // second argument is what to replace with. 
-                        // In .replace(/\s/g, '')  we are replacing all spaces with '_'
-                        // '\s'  is a regular expression for space, which needs to be inside /<regular expression>/ 
-                        // and'g' denotes for global (that means all spaces), if you dont give 'g' there, 
-                        // it will replace the first space (only one space) and will replaced by '_' 
-                        // In review page .replace(/_/g, ' ') replaces all underscores with space
-                        
-                        return   (
-                        
-                        //We only need to find the id for each ship, because the api is doing the rest of the work for us by finding the 
-                        //correct Cruise line to match the id. 
-                        <li key = {ship.id}>
-                            
-                            {/* Link in Cruise Lines Page to Review Page now works, because we have used encoding referenced from constants above. 
-                            So we now have a HashLink with a hash and underscore in it.   */}
-                            <Link className = "shipsList" smooth to = {`/cruiselines/${encodedCruiseline}#${encodedShipName}`}
-                             onMouseOut = {this.onMouseOut} onMouseOver = {this.onMouseOver}>
+                    {this.findShipNames(ship)}
+                </Link>  */}
 
-                            {this.findShipNames(ship)}
-                            </Link>
-                        </li>
-                        )  
-
-                    //This Link has a className, because I want to style fonts using CSS for Cruise Ship Names. 
+                <Link className = {this.state.isHover ? "isHover" : shipsList}
+                    smooth
+                    to = {`/cruiselines/${this.props.encodedCruiseline}#${this.props.encodedShipName}`}
+                    onMouseOut = {this.onMouseOut}
+                    onMouseOver = {this.onMouseOver}>
+                        {this.props.ShipName}
+                </Link>
+                    {/* //This Link has a className, because I want to style fonts using CSS for Cruise Ship Names. 
                     //The key is used to get correct ship names by id for each Ships List, when a Cruise Line Heading is clicked.
                     //The Link will be an Anchor Link to a specific ship, on it's Cruise Line Page. 
                     //The Link will have a smooth feature, so it scrolls to the correct ship on it's Cruise Line Page. 
-                    // Note: The Cruise Line Page, can have a Review for more than one ship and will be Review and Ship Components.              
-                })
-                }
-                </ol>
+                    // Note: The Cruise Line Page, can have a Review for more than one ship and will be Review and Ship Components.*/}
+              
 
             </React.Fragment> 
         )
--- client/components/ListofShips.jsx
+++ client/components/ListofShips.jsx
@@ -18,6 +18,8 @@ class ListofShips extends React.Component {
       }
       //binding method for setting up Ships in their List for their Cruise Line
       this.setUpShips = this.setUpShips.bind(this)
+      // binding method for HandClick function, which controls clicking on a Cruise Line Heading
+      this.handleClick = this.handleClick.bind.this
     }
    
     componentDidMount(){
@@ -46,13 +48,48 @@ class ListofShips extends React.Component {
     
       <React.Fragment>
 
-        <HandleClick/>
-        <HoverList/>
+        <div>
+          <HandleClick onclick={this.handleClick} cruise_line={this.props.cruise_line}/>
+        </div>
+          
+
+        <ol>
+                    
+          {/* When Ships List is about to be opened, we will use map to find the correct Ships for the List.*/}
+          {this.state.isOpen == true && 
+            this.state.shipsList.map (ship => { 
+                        
+              // Below we are creating constants that will be used in the Link below.
+
+              // The CruiseLine and ShipName constants are used to replace all spaces in the url with an underscore "_".
+              const encodedCruiseline = ship.cruise_line.replace(/\s/g, '_')
+              const encodedShipName = ship.ship_name.replace(/\s/g, '_')
+
+              // replace function takes two arguments, first argument for matching pattern (what to replace) and 
+              // second argument is what to replace with. 
+              // In .replace(/\s/g, '')  we are replacing all spaces with '_'
+              // '\s'  is a regular expression for space, which needs to be inside /<regular expression>/ 
+              // and'g' denotes for global (that means all spaces), if you dont give 'g' there, 
+              // it will replace the first space (only one space) and will replaced by '_' 
+              // In review page .replace(/_/g, ' ') replaces all underscores with space
+                        
+              return   (
+                        
+              //We only need to find the id for each ship, because the api is doing the rest of the work for us by finding the 
+              //correct Cruise line to match the id. 
+              <li key = {ship.id}>
+                            
+                <HoverList encodedCruiseline={cruise_line} encodedShipName={ship_name}/>
+
+              </li>
+                        )                
+                })
+                }
+                </ol>
 
       </React.Fragment>
     )  
-  }
-  
+  } 
 }
 
 export default ListofShips
