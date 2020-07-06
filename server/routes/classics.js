7cbbdc89b4ddaa69adec2ea9d7c1b2a2fc56fcea
Robert Wilkinson <robjwilkinson@hotmail.com>
2020-07-06 22:01:55 +1200

    New Route - Classics for displaying Ships on Evolution Pages

---
+++ server/routes/classics.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const db = require('../db/dbclassics')
+const router = express.Router()
+router.use(express.json())
+
+
+router.get('/', (req, res) => {
+    db.getClassics()
+    .then(classics => 
+        res.json(classics))
+    })
+
+
+module.exports = router
