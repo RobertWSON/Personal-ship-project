d4fbe139a113842b0f4a42309df3915d552474c0
Robert Wilkinson <robjwilkinson@hotmail.com>
2020-07-14 15:19:50 +1200

    evo2 migration data for Evolution Part 2

--- migrations/20200619230127_evo2.js
+++ migrations/20200619230127_evo2.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('Evo2', table => {
+        table.increments('id').primary()
+        table.string('ship_name')
+        table.string('img')
+        table.string('img_title')
+        
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('Evo2')
 };
