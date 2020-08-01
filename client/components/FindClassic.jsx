ed0233cce422972ccfcc6479be9dce6eab8f32dc
malyz <mattuy001@gmail.com>
2020-08-02 11:54:17 +1200

    FIXED: missing / in index.html, added fetch ship if user entered url address manually, added first() in getClassic

--- client/components/FindClassic.jsx
+++ client/components/FindClassic.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import MakeClassic from './MakeClassic'
+import { getClassic } from '../api/api'
 
 class FindClassic extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      classicship: null
+    }
+    this.setUpClassic = this.setUpClassic.bind(this)
+  }
+
+  componentDidMount() {
+    const { location, match } = this.props
+    if (location.state) {
+      this.setState({ classicship: location.state })
+    } else {
+      this.setUpClassic(match.params.shipname)
+    }
+  }
+
+  setUpClassic(shipName) {
+    getClassic(shipName)
+      .then(ship => {
+        this.setState({
+          classicship: ship
+        })
+      })
+  }
+
   render() {
-    return <MakeClassic {...this.props.location.state} />
+    if (!this.state.classicship) return <div>Loading...</div> //Create your loading component
+    return <MakeClassic {...this.state.classicship} />
   }
 }
 
