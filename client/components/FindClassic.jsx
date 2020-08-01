c6b3ccdf013ea25891c8b9a0857c1a82cf30227e
malyz <mattuy001@gmail.com>
2020-08-02 09:19:13 +1200

    FIXED: show details in FindClassic component

--- client/components/FindClassic.jsx
+++ client/components/FindClassic.jsx
@@ -1,52 +1,10 @@
 import React from 'react'
 import MakeClassic from './MakeClassic'
-import {getClassic} from '../api/api'
 
-class FindClassic extends React.Component   {
-
-    constructor(props)  {
-        super(props)
-
-        this.state = {
-            
-            //initialize classicShip as an array from evo1 table.
-            classicship: ['evo1']
-        }
-        this.setUpClassic = this.setUpClassic.bind(this)
-    }
-    
-    componentDidMount() {
-        console.log('cdm')
-        this.setUpClassic()
-    }
-
-    setUpClassic()  {
-        getClassic(this.state.classicship)
-
-        .then(res =>    {
-            this.setState({
-                classicship: res
-            })
-        })
-    }
-
-    render()    {
-
-        return  (
-            <React.Fragment>
-
-            {this.state.classicship.map (ship => {
-                return  (
-
-                    <MakeClassic key = {ship.id} {...ship}/>
-                   
-                )
-            }
-            )}
-
-            </React.Fragment>
-        )
-    }
+class FindClassic extends React.Component {
+  render() {
+    return <MakeClassic {...this.props.location.state} />
+  }
 }
 
 export default FindClassic
