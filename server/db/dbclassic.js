92bc638ea7331b572e10cbf4e6487e4fda79f99b
Robert Wilkinson <robjwilkinson@hotmail.com>
2020-10-26 00:33:05 +1300

    Trying to make Evolution Part 2 Page show the correct ships on that page, but now I am getting a this.state.classicShips.map is not a function error for Evolution Part 1 and 2 pages and nothing is showing

--- server/db/dbclassic.js
+++ server/db/dbclassic.js
@@ -4,32 +4,49 @@ const db = require('knex')(config)
 
 
 function getClassic(classic_ship_name, testConn)    {
-// function getClassic(ship_name, testConn)    {
+
     const conn = testConn || db 
-    // console.log('server/db/dbclassic.js getClassic called')
+    console.log('server/db/dbclassic.js getClassicc called!')   
+    
+    // Initialize evo1 table as a starting point for displaying data
+    let evo1
 
-    // Line below connects to Evo1 Table for Evolution Part 1 and 
-    // then Evo2 Table for Evolution Part 2 
-    return conn('Evo1','Evo2')
-    //Line below picks out each distinct Ship Name from Evo1 Database
-    // .distinct('ship_name')
-    // I have a feeling that distinct may not be correct to use here
-     
-    // .first() method returns only the first result, which is an object not an Array.
-    // .first()
-    // .select(ship_name, img, img_title)
+    // Line below connects to Evo1 Table for Evolution Part 1 
+    return conn('Evo1')
 
-    // I am going to give where a try, it may work
-    // .where('ship_name', ship_name)
-    .where('classic_ship_name', classic_ship_name)
-    //Line below picks out ship names in alphabetical order by id
-    .orderBy('id')
-    // .first() method returns only the first result, which is an object not an Array.
-    .first()
-    // .then(dbResult =>  {
-    //     console.log('dbclassic.js - GOT CLASSIC SHIP:', dbResult)
-    //     return dbResult
-    // }) 
+        // I am going to give where a try, it may work
+        // I think where is the appropriate method to use here
+        .where('classic_ship_name', classic_ship_name)
+    
+        //Line below picks out ship names in alphabetical order by id
+        .orderBy('id')
+    
+        // .first() method returns only the first result, which is an object not an Array.
+        .first()
+        // Uncommented below to see what happens with regards to displaying Evolution Part 2 page correctly.
+        // Now commented at 12:53 am on Sunday because 
+        // .then(dbResult =>  {
+        //     console.log('dbclassic.js - GOT CLASSIC SHIP:', dbResult)
+        //     return dbResult
+        // })
+        .then((EvooneResults) => {
+            console.log('dbclassic.js - GOT CLASSIC SHIP:', EvooneResults)
+        Evo1 = EvooneResults
+    
+    // Line below connects to Evo2 Table for Evolution Part 2        
+    return conn('Evo2')
+    
+        .where('classic_ship_name', classic_ship_name)
+        .orderBy('id') 
+        .first()
+    })
+        .then((EvoTwoResults) => {
+        console.log('dbclassic.js - GOT CLASSIC SHIP:', EvoTwoResults)
+    return  {        
+        evo1,
+        evo2: EvoTwoResults
+    }           
+    })
 }
 
 module.exports = {
--- server/db/dbclassics.js
+++ server/db/dbclassics.js
@@ -3,27 +3,40 @@ const config = require('../../knexfile')[environment]
 const db = require('knex')(config)
 
 
-// function getClassics (testConn)  {
-// function getClassics (id, testConn)  { 
-//id doesn't work, so I will try ship_name
-// function getClassics (ship_name, testConn)   { 
 function getClassics(testConn)  {
 
     const conn = testConn || db
     console.log('server/db/dbclassics.js getClassics called!')
 
-    // Line below connects to Evo1 Table for Evolution Part 1 
-    // and then Evo2 Table for Evolution Part 2 
-    return conn('Evo1','Evo2')
-    // Line below picks out ships to display from Evo1 Database.
-    .select()
-    // .select(ship_name)
-    // .select(id)
-    //Line below displays classic ships in id order.
-    .orderBy('id')
-    .then(dbResult  =>  {
-        console.log('dbclassics.js - GOT CLASSIC SHIPS:',dbResult)
-        return dbResult
+    // Initialize evo1 table as a starting point for displaying data
+    let evo1
+    // Line below connects to Evo1 Table for Evolution Part 1  
+    return conn('Evo1')
+        // Line below picks out ships to display from Evo1 Database.
+        .select()
+        //Line below displays classic ships in id order.
+        .orderBy('id')
+        .then((EvooneResults) => {
+            console.log('dbclassics.js - GOT CLASSIC SHIPS:', EvooneResults)
+        Evo1 = EvooneResults
+
+    // Line below connects to Evo2 Table for Evolution Part 2        
+    return conn('Evo2')
+        // Line below picks out ships to display from Evo2 Database.        
+        .select()
+        //Line below displays classic ships in id order.        
+        .orderBy('id')    
+    })
+        .then((EvoTwoResults) => {
+            console.log('dbclassics.js - GOT CLASSIC SHIPS:', EvoTwoResults)
+    return  {        
+        evo1,
+        evo2: EvoTwoResults
+    }    
+    // Commented original below because I am trying this new method above
+    // .then(dbResult  =>  {
+    //     console.log('dbclassics.js - GOT CLASSIC SHIPS:',dbResult)
+    //     return dbResult
     })
 }
 
