76d78d1e912c8170d2171d2ce5e66d6c6823b0ae
Robert Wilkinson <robjwilkinson@hotmail.com>
2020-11-03 00:17:30 +1300

    dbclassics & dbclassic - Experimenting by Taking out final return and let evo1 to see localhost change and in EvolutionPart1 and EvoPart2 experimenting with classicShips res as well to see localhost change to nothing displaying. I have gone back to what was working after this.

--- server/db/dbclassic.js
+++ server/db/dbclassic.js
@@ -6,8 +6,9 @@ const db = require('knex')(config)
 function getClassic(classic_ship_name, testConn)    {
 
     const conn = testConn || db 
-    console.log('server/db/dbclassic.js getClassicc called!')   
+    console.log('server/db/dbclassic.js getClassic called!')   
     
+    // I may not need let evo1 because seems like it doesn't make a difference with localhost
     // Initialize evo1 table as a starting point for displaying data
     let evo1
 
@@ -18,35 +19,35 @@ function getClassic(classic_ship_name, testConn)    {
         // I think where is the appropriate method to use here
         .where('classic_ship_name', classic_ship_name)
     
-        //Line below picks out ship names in alphabetical order by id
+        //Line below picks out Classic Ship Names in alphabetical order by id
         .orderBy('id')
     
         // .first() method returns only the first result, which is an object not an Array.
         .first()
-        // Uncommented below to see what happens with regards to displaying Evolution Part 2 page correctly.
-        // Now commented at 12:53 am on Sunday because 
-        // .then(dbResult =>  {
-        //     console.log('dbclassic.js - GOT CLASSIC SHIP:', dbResult)
-        //     return dbResult
-        // })
+         
+        //This picks up the information for a Classic Ship page eg Titanic in Evolution Part 1
         .then((EvoOneResults) => {
             console.log('dbclassic.js - GOT CLASSIC SHIP:', EvoOneResults)
         evo1 = EvoOneResults
-    
-    // Line below connects to Evo2 Table for Evolution Part 2        
+   
+    // Line below connects to Evo2 Table for Evolution Part 2         
     return conn('Evo2')
     
         .where('classic_ship_name', classic_ship_name)
         .orderBy('id') 
         .first()
     })
+    // This picks up the information for a Classic Ship page eg Fairstar in Evolution Part 2
         .then((EvoTwoResults) => {
         console.log('dbclassic.js - GOT CLASSIC SHIP:', EvoTwoResults)
+    
+    // This displays information for a Classic Ship, taken from either evo1 or evo2 seeds     
     return  {        
         evo1,
         evo2: EvoTwoResults
     }           
     })
+
 }
 
 module.exports = {
