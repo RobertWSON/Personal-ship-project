e2f5619766571380a047022a4758a64b08621e35
Robert Wilkinson <robjwilkinson@hotmail.com>
2020-11-03 21:00:13 +1300

    Now have EvolutionPart2 instead of EvoPart2 Component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,23 +4,21 @@ import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 import Nav from './Nav'
 import Home from './Home'
+
 // Evolution pages (Part1)
 import EvolutionPart1 from './EvolutionPart1'
 import FindClassic from './FindClassic'
-
 // Evolution pages (Part2)
-import EvoPart2 from './EvoPart2'
+import EvolutionPart2 from './EvolutionPart2'
 import FindClassicPt2 from './FindClassicPt2'
-// import Michelangelo from './Michelangelo'
-// import Qe2 from './Qe2'
-// import Fairstar from './Fairstar'
-// import SongofNorway from './SongofNorway'
-// import RoyalVikingSky from './RoyalVikingSky'
-// import AllureoftheSeas from './AllureoftheSeas'
 
+// Cruise Lines page
 import CruiseLines from './CruiseLines'
+// Cruise Ship Review page
 import Review from './Review'
+// A Ship Terms page to help understand all ship terms
 import ShipTerms from './ShipTerms'
+
 
 class App extends React.Component {
   constructor(props)  {
@@ -31,48 +29,28 @@ class App extends React.Component {
     return (
       <BrowserRouter>
     
-          <div>
-            
+          <div>   
               <Route path='/' component={Nav}/>
               <Route exact path='/' component={Home}/>
 
-              {/* Routes for Evolution pages and associated ships */}
+              {/* Routes for Evolution pages and associated ships  */}
               <Route exact path='/evolutionpart1' component={EvolutionPart1}/>
               <Route path = '/evolutionpart1/:classic_ship_name' component={FindClassic}/>
-
-              <Route exact path='/evolutionpart2' component={EvoPart2}/>
+              <Route exact path='/evolutionpart2' component={EvolutionPart2}/>
               <Route path = '/evolutionpart2/:classic_ship_name' component={FindClassicPt2}/>
-              {/* <Route exact path='/evolutionpart2/michelangelo' component={Michelangelo}/> */}
-              {/* <Route exact path='/evolutionpart2/qe2' component={Qe2}/> */}
-              {/* <Route exact path='/evolutionpart2/fairstar' component={Fairstar}/> */}
-              {/* <Route exact path='/evolutionpart2/songofnorway' component={SongofNorway}/> */}
-              {/* <Route exact path='/evolutionpart2/royalvikingsky' component={RoyalVikingSky}/> */}
-              {/* <Route exact path='/evolutionpart2/allureoftheseas' component={AllureoftheSeas}/> */}
 
               {/* Route for Cruise Lines page */}
               <Route exact path='/cruiselines' component={CruiseLines}/>
 
-              
+              {/* Route for the Review of each ship on it's Cruise Line page.  
+              Recommended by Ricky Sullivan from slack, as it passes a param. */}
               <Route path = '/cruiselines/:cruise_line' component={Review}/>
-              {/* Route below is recommended by Ricky Sullivan from slack, as it passes a param.
-              The problem is that it just doesn't work at this stage, probably because the backend and api needs to be sorted. 
-              <Route path = '/cruiselines/:cruise_line' component={Review}/> */}
-
-              {/* Route for Ship Component that controls the details we are getting for the ship
-              and then returns Cruise Line and Correct Ships in Review Component. */}
-
-              {/* Commented out because this Ship route does not work */}
-              {/* <Route path = '/cruiselines/:cruise_line' component={Ship}/> */}
-
-              {/* Ship Term Route Added */}
+        
+              {/* Ship Term Route Added, but not going to a new page yet. */}
               <Route path = '/cruiselines/:cruise_line/shipterms' component={ShipTerms}/>
 
-              {/* <Route exact path='/booking' component={Booking}/> */}
-              
-            {/* </div> */}
           </div>  
-        {/* </React.Fragment>   */}
-      {/* </Router>  */}
+
       </BrowserRouter>
     )
   }
--- client/components/EvolutionPart2.jsx
+++ client/components/EvolutionPart2.jsx
@@ -1,117 +1,87 @@
-// import React from 'react'
-// import {Link} from 'react-router-dom'
+import React from 'react'
+import {getClassics} from '../api/api'
+import ClassicShips2 from './ClassicShips2'
 
+
+class EvolutionPart2 extends React.Component    {
     
-// class EvolutionPart2 extends React.Component    {
+    constructor(props)  {
+        super(props)
+
+        this.state = {      
+            // later I will have to work out how the data can come from the correct table eg evo1 or evo2.
+            //initialize classicShip as false to allow for no data.
+            classicShips: false        
+        }
+
+        this.setUpClassics = this.setUpClassics.bind(this)
+    }    
+
+    componentDidMount() {
+        console.log('cdm')
+        this.setUpClassics()
+    }
+
+    setUpClassics() {
+        console.log('getClassics')
+        //Line below gets the getClassics function from the api.
+        getClassics()
+        //.then get a response from finding ships from database.
+        .then(res =>    {
+
+            this.setState({
+                // Returns response from api
+                classicShips: res.evo2
+            })
+        })
+    }
     
-//     constructor(props)  {
-        
-//         super(props)
-//     }    
+    render()    { 
+        //console for testing if data is coming through.
+        console.log(this.state.classicShips)
+        // Line below allows for classicships being false and no data is appearing, so it says Loading...
+        if(!this.state.classicShips) return <div>Loading...</div>
+        //Else display the following elements below on the page.
+        return  (
+            <div>
+                <div className = "header">
+                    <h1>Evolution of Cruise Ships</h1> 
+                </div>
 
-//     render()    { 
-        
+                <div className = "evoContainer">
+                    {/* Map classicships in state to find a ship */}
+                    {this.state.classicShips.map (ship =>  {
+                        // Console for testing if a ship (classic one) is being mapped for the correct one
+                        console.log(ship)
+                        return  (
 
-//         return (
-//             <div>
-                
-//                 <div className = "header">
-//                     <h1>Evolution of Cruise Ships</h1> 
-//                 </div>
-                    
-//                 <div className = "evoContainer">
-//                     <div className = "evoShips">  
-//                         <h3>Fairstar</h3>
-//                         <Link to = "/evolutionpart2/fairstar">
-//                             <img src={"/images/Evolution/Fairstar/Fairstar01.jpg"}
-//                             title = "Fairstar cruising off Pacific Islands in early 90's"/>
-//                         </Link>
+                            // Returning a div for each classic ship based on id and in id order
+                            <div key = {ship.id} className = "evoShips">     
+                                <ClassicShips2 {...ship} />
+                            </div>
+                        )
+                    }
+                    )} 
 
-                        /* <br/> 
-                        <p>  */
+                <div className = "evoIntro">
+                        <h3>Last of the Classics</h3>
+                        <p>The start of this <b>evolution (Part 2)</b> treats you to a real classic Fairstar, that was originally 
+                        operated as a troopship and got converted completely to a cruise ship. <br/>I am fortunate enough to have been on this ship during the 90's, when I was a young lad.
+                        <br/><br/>The Michelangelo was an Italian classic liner, which earns it classic because of it's capped laticed funnels. 
+                        <br/><br/>QE2 or as it is more formally known Queen Elizabeth 2, named after the queen is one of the most well known cruise ships of all time.
+                        Operated under Cunard it's entire career, she is now still around, but as a floating hotel in Dubai.
+                        <br/><br/>Song of Norway goes down as the first of the classic liners ever to be lengthend to allow more passengers.
+                        Song of Norway has had lots of differents name during it's career, is scrapped now, but will be rememembered as a great cruise ship. 
+                        <br/><br/>Royal Viking Sky is another classic cruise ship that got lengthened, but it is still in operation today and can be seen on my Cruise Lines page for Fred Olsen cruises.
+                        <br/><br/>Lastly Allure of the Seas is a different type of cruise ship from the other classic ones I have in the evolution pages.
+                        Being the box shape it is, it is designed to carry more passengers and is a lot larger than the other ships.
+                        It is also available for cruises on my Cruise Lines page for Royal Caribbean cruises. 
+                        </p>    
+                    </div>
+                </div>       
+            </div>            
+        )
+    }
+}
 
-                        // <h4>Find out more on Fairstar, Click on Image</h4>
-
-                        /* </p> 
-                        <br/>  */
-
-                    //     <p>Fairstar Ship Horn</p>
-                    //     <br/>
-                    // </div>    
-
-                    // <div className = "evoShips">
-                    //      <h3>Michelangelo</h3>
-                    //      <Link to = "evolutionpart2/michelangelo">
-                    //          <img src={"/images/Evolution/Michelangelo/michelangelo_1965_1.jpg"}
-                    //         title = "Michelangelo cruising off Canary Islands in 1965"/>
-                    //     </Link>                        
-                    //     <p>Find out more on Michelangelo, Click on Image</p>                    
-                    //     <p>Michelangelo Ship Horn</p>
-                    // </div>    
-
-                    // <div className = "evoShips">
-                    //     <h3>Qe2</h3>
-                    //     <Link to = "evolutionpart2/qe2">
-                    //         <img src={"/images/Evolution/Qe2/5148955_orig.jpg"}
-                    //         title = " Queen Elizabeth 2 in original colours, cruising off Canary Isles "/>
-                    //     </Link>                   
-                    //     <p>Find out more on Qe2, Click on Image</p>                        
-                    //     <p>Qe2 Ship Horn</p>
-                    //  </div>
-
-                    //  <div className = "evoShips">    
-                    //      <h3>Song of Norway</h3>
-                    //      <Link to = "evolutionpart2/songofnorway">
-                    //          <img src={"/images/image_manager__rex_thumbnail_images_song_of_norway_08.06.1994.jpg"}
-                    //         title = "Song of Norway in Panama Canal 1994"/>
-                    //     </Link>
-                    //     <p>Find out more on Song of Norway, Click on Image</p>
-                    //     <p>Song of Norway Ship Horn</p>
-                    // </div>    
-
-                    // <div className = "evoShips">
-                    //     <h3>Royal Viking Sky</h3>
-                    //     <Link to = "/evolutionpart2/royalvikingsky">
-                    //         <img src={"/images/1757063.jpg"}
-                    //         title = "Royal Viking Sky in Picton, New Zealand, March 1982"/>
-                    //     </Link>
-                    //     <p>Find out more on Royal Viking Sky, Click on Image</p>
-                    //     <p>Royal Viking Sky Ship Horn</p>
-                    // </div>
-
-                    // <div className = "evoShips">
-                    //     <h3>Allure of the Seas</h3>
-                    //     <Link to = "/evolutionpart2/allureoftheseas">
-                    //         <img src={"/images/UFRPU.jpg"}
-                    //         title = "Allure of the Seas maiden voyage celebration"/>
-                    //     </Link>
-                    //     <p>Find out more on Allure of the Seas, Click on Image</p>
-                    //     <p>Allure of the Seas Ship Horn</p>                        
-                    // </div>
-
-                    // <div className = "evoIntro">
-
-                    /* <h3 className = "Endings">History (Last of the Classics)</h3>  */
-
-//                         <h3>Last of the Classics</h3>
-//                         <p>The start of this <b>evolution (Part 2)</b> treats you to a real classic Fairstar, that was originally 
-//                         operated as a troopship and got converted completely to a cruise ship. <br/>I am fortunate enough to have been on this ship during the 90's, when I was a young lad.
-//                         <br/><br/>The Michelangelo was an Italian classic liner, which earns it classic because of it's capped laticed funnels. 
-//                         <br/><br/>QE2 or as it is more formally known Queen Elizabeth 2, named after the queen is one of the most well known cruise ships of all time.
-//                         Operated under Cunard it's entire career, she is now still around, but as a floating hotel in Dubai.
-//                         <br/><br/>Song of Norway goes down as the first of the classic liners ever to be lengthend to allow more passengers.
-//                         Song of Norway has had lots of differents name during it's career, is scrapped now, but will be rememembered as a great cruise ship. 
-//                         <br/><br/>Royal Viking Sky is another classic cruise ship that got lengthened, but it is still in operation today and can be seen on my Cruise Lines page for Fred Olsen cruises.
-//                         <br/><br/>Lastly Allure of the Seas is a different type of cruise ship from the other classic ones I have in the evolution pages.
-//                         Being the box shape it is, it is designed to carry more passengers and is a lot larger than the other ships.
-//                         It is also available for cruises on my Cruise Lines page for Royal Caribbean cruises. 
-//                         </p>    
-//                     </div>
-//                 </div>       
-//             </div>            
-//         )
-//     }
-// }
-
-
-// export default EvolutionPart2
+export default EvolutionPart2
