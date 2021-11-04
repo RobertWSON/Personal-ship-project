eeefd6998b117d18dd12016a52f7728b8e89a86d
Robert Wilkinson <robjwilkinson@hotmail.com>
2021-11-04 16:12:50 +1300

    dbreviews - joins code slightly changed with = and ship_id

--- server/db/dbreviews.js
+++ server/db/dbreviews.js
@@ -7,11 +7,20 @@ function getReviews()   {
     const db = testConn || db
     console.log('server/db/dbreviews.js getReviews called!')
 
+// return db('ships')
+//     .join('reviews','ships.id', '=', 'reviews.ship_id')
+//     .where('ship_id', '=', id)
+//     .select("*", "ships.id AS sid", "reviews.id AS rid")
+//     .first()
+
+//Trial new joins code 4th Nov 2021    
 return db('ships')
-    .join('reviews','ships.id', '=', 'reviews.ship_id')
-    .where('ship_id', '=', id)
-    .select("*", "ships.id AS sid", "reviews.id AS rid")
+    .join('reviews','ships.id', 'reviews.ship_id')
+    .where({ship_id:id})
+    .select("*", "ships.id AS sid", "reviews.ship_id AS rid")
     .first()
+
+
     .then(record => {
         console.log(record)
         console.log('Ship' + record.Review_User_Name + 'has a' + record.Review + ' ' + record.StarRating + '' + record.Cruise_Name 
